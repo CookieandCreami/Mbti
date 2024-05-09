20876025f75b750f7d24e8bd958276e5a6cf4ff6
CookieandCreami <163944573+CookieandCreami@users.noreply.github.com>
2024-05-09 17:52:36 +0900

    최초

--- app/src/main/java/com/example/mbti/MainActivity.kt
+++ app/src/main/java/com/example/mbti/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -42,16 +42,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun MyMbti() {
     Column {
-
-        Text(text = "저의 mbti는 isfj입니다.", fontSize = 15.sp)
-        Image(
-            painter = painterResource(id = R.drawable.mbti),
-            contentDescription = "mbti"
-        )
-
+        Text(text = "저의 mbti를 소개합니다.", fontSize = 15.sp)
+        MbtiImage()
     }
 }
 
+@Composable
+fun MbtiImage() {
+    Image(
+        painter = painterResource(id = R.drawable.mbti2),
+        contentDescription = "mbti",
+        contentScale = ContentScale.Inside
+    )
+
+}
 
 @Preview(showBackground = true)
 @Composable
